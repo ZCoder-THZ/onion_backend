05419277caec2736d477f425fd30be3502a85c0e
Than Htike Zaw <thanhtikezaw919@gmail.com>
2024-01-18 10:04:35 -0600

    database changes

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -1,20 +1,20 @@
 const prisma = require('../config/db.config');
 
-exports.getIncomes=async (req,res)=>{
+exports.getIncomes = async (req, res) => {
 
-    const incomes=await prisma.incomes.findMany();
+    const incomes = await prisma.incomes.findMany();
 
 
     return res.status(200).json({
-        message:'expense',data:incomes
+        message: 'expense', data: incomes
     })
 
 }
-exports.createIncome=async (req,res)=>{
+exports.createIncome = async (req, res) => {
 
-    const incomes=await prisma.incomes.create({data:req.body});
+    const incomes = await prisma.incomes.create({ data: req.body });
 
     return res.json({
-        message:'created successfully',data:incomes
+        message: 'created successfully', data: incomes
     })
 }
--- routes/incomes/incomes.js
+++ routes/incomes/incomes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth')
 const { getIncomes, createIncome } = require('../../controllers/incomeController');
 router.route('/').get(getIncomes).post(createIncome)
 // router.route('/:id').put(updateExpense).delete(deleteExpense).get(getExpenseById)
